7df6fb049d9654f9c41b19827bf33286e8f14ef2
lscoupling <e0975551630@outlook.com>
2025-01-02 23:03:42 +0800

    Some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,22 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:exam_demo/screens/login_screen.dart';
-import 'package:exam_demo/screens/question_screen.dart';
+import 'screens/login_screen.dart';
 
-class ExamSelectionScreen extends StatelessWidget {
-  const ExamSelectionScreen({Key? key}) : super(key: key);
+void main() {
+  runApp(const MyApp());
+}
 
-  void _navigateToExam(BuildContext context, String subject) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (_) => QuestionScreen(subject: subject),
-      ),
-    );
-  }
-
-  // 登出：導回 LoginScreen 並清除路由
-  void _logout(BuildContext context) {
-    Navigator.pushAndRemoveUntil(
-      context,
-      MaterialPageRoute(builder: (_) => const LoginScreen()),
-      (route) => false,
-    );
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('考科選擇'),
+    return MaterialApp(
+      title: 'Exam Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () => _navigateToExam(context, 'SAA'),
-              child: const Text('SAA'),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () => _navigateToExam(context, 'GCP'),
-              child: const Text('GCP'),
-            ),
-          ],
-        ),
-      ),
-
-      // ★ 將登出按鈕放在最下方 (bottomNavigationBar) ★
-      bottomNavigationBar: Container(
-        color: Colors.white,
-        padding: const EdgeInsets.symmetric(vertical: 16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton.icon(
-              icon: const Icon(Icons.exit_to_app),
-              label: const Text('登出'),
-              onPressed: () => _logout(context),
-            ),
-          ],
-        ),
-      ),
+      home: const LoginScreen(),
     );
   }
 }
